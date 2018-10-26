c04484417602dc62810cf03d8d38ee9574604d80
samarv <samarvir1996@gmail.com>
2018-10-27 05:02:11 +0530

    add tabs

--- js/index.js
+++ js/index.js
@@ -16,3 +16,20 @@ for(i=0;i<bylineArr.length;i++){									// Loop for every letter
   	byline.appendChild(span); 					// Add the span to the h2
   }
 }
+
+function openCity(cityName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(cityName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
