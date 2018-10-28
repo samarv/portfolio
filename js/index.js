4e0d87e1fec3287cc03c9730a19eedc4c13b8eca
IshwantSingh <ishwant2001@gmail.com>
2018-10-29 02:23:25 +0530

    basic work done

--- js/index.js
+++ js/index.js
@@ -16,3 +16,107 @@ for(i=0;i<bylineArr.length;i++){									// Loop for every letter
   	byline.appendChild(span); 					// Add the span to the h2
   }
 }
+
+//Making pages
+
+class Nav {
+  constructor(nav) {
+    this.navItem = nav;
+    this.navItem.addEventListener("click", () => {
+      this.navClick();
+    });
+    this.navData = this.navItem.dataset.tab;
+
+    this.Content1 = document.querySelector(`.content[data-tab="${this.navData}"]`);
+
+    this.tabcontent = document.querySelector('.tab-content')
+
+    console.log("tc", this.Content1)
+    
+    this.Content1 = new Content(this.Content1);
+
+  };
+  //Methods
+  navClick() {
+    this.navItem.parentNode.parentNode.classList.add('show-content');
+    this.navItem.parentNode.parentNode.classList.replace('show-content','hide-content');
+    this.Content1.showContent();
+    this.Content1.tabLook();
+  };
+
+};
+//2nd class
+
+class Content {
+  constructor(Content1) {
+    this.Content1 = Content1;
+
+  }
+  //methods
+  showContent() {
+    console.log(this.Content1)
+    this.Content1.classList.add('hide-content');
+    this.Content1.classList.replace('hide-content','show-content');
+  }
+  tabLook() {
+    this.Content1.classList.toggle('tab-look');
+  }
+ 
+}
+
+
+let navs = document.querySelectorAll(".navItem");
+navs = Array.from(navs).map(navItem => new Nav(navItem));
+
+
+//tryin' somethin'
+
+class CloseItem {
+  constructor(close2) {
+    this.close = close2;
+    this.close.addEventListener("click", () => {
+      this.closeClick();
+    });
+
+  };
+  //Methods
+  closeClick() {
+    hide(this.close.parentNode);
+    show(mainContent);
+    remove(this.close.parentNode);
+  };
+
+};
+
+
+//references
+const mainContent = document.querySelector('.main-content');
+console.log(mainContent);
+let close3 = document.querySelectorAll('.close');
+close3 = Array.from(close3).map(close => new CloseItem(close));
+const tabCon = document.querySelector('.tab-content');
+// const con = document.querySelector('.content');
+// console.log(con)
+
+// close.addEventListener("click", () => {
+//   hide(con);
+//   show(mainContent);
+//   remove(con);
+//   console.log('it works yeah');
+// });
+// close.addEventListener('mouseover', () => {
+
+// })
+
+function hide(classRefer) {
+  classRefer.classList.replace('show-content','hide-content');
+};
+
+function remove(classRef) {
+  classRef.classList.remove('tab-look');
+}
+function show(className) {
+   className.classList.replace('hide-content','show-content');
+  };
+
+
